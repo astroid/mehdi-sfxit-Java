fbba8bf84a494a5482b1d3870a54aae16f080a3a
astroid <mehdibsh@gmail.com>
2015-02-13 18:37:08 +0100

    homework

--- src/prject1/MaxOcurrence.java
+++ src/prject1/MaxOcurrence.java
@@ -1,5 +1,7 @@
 package prject1;
 
+import java.util.Scanner;
+
 /*
  * Write a program which finds the most frequently occurring element in an array. 
  * Example: {4, 1, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9, 3}. Result: 4 (5 times).
@@ -8,8 +10,18 @@ package prject1;
 public class MaxOcurrence {
 
 	public static void main(String[] args) {
-		int[] arr = { 1, 3, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, 7 };
-		maxOcr(arr);
+		// int[] myArray = {4, 1, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9, 3};
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter The Sizes of the array : ");
+		int size = input.nextInt();
+
+		int myArray[] = new int[size];
+		System.out.printf("Enter  the elements of the array :");
+		for (int i = 0; i < size; i++) {
+			myArray[i] = input.nextInt();
+		}
+		input.close();
+		maxOcr(myArray);
 	}
 
 	public static int maxOcr(int[] array) {
